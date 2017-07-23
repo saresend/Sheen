ee88526018416a8c1219a09323b0d8c1edd45da6
Samuel Resendez <saresend@usc.edu>
2017-07-23 21:15:25 +0300

    simplified interface

--- fileParser.py
+++ fileParser.py
@@ -1,25 +1,40 @@
 import re
 import os
+import sys
 
 def parseFile(file_path):
+    foundTodo = False
     f = open(file_path, 'r')
     todoReg = re.compile(r'(.*)(TODO.+)')
     count = 0
     for line in f:
         match = todoReg.match(line)
         if match != None:
+            foundTodo = True
             print('\tLine ' + str(count) + ': ' + match.group(2))
         count += 1
 
+    if foundTodo == False:
+        print("No TODOs Found")
+
 
 def scan_directory(dirName):
-    directory = os.scandir(dirName)
-    for fileObj in directory:
-        print(fileObj.path)
-        parseFile(fileObj.path)
+    try:
+        directory = os.scandir(dirName)
+        for fileObj in directory:
+            if fileObj.is_dir():
+                scan_directory(fileObj.path)
+            elif fileObj.is_file():
+                print(fileObj.path)
+                parseFile(fileObj.path)
+
+    except NotADirectoryError as e:
+        parseFile(dirName)
     
 if __name__ == "__main__":
-    # parseFile('TwitterAgent.py')
-    scan_directory('testDir')
-            
+    if len(sys.argv) < 2:
+        print("Please enter file or directory name")
+        exit()
+    else: 
+        scan_directory(sys.argv[1])
     
