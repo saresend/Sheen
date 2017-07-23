2a531618c2145e4213480c8ce5d67ba94894af98
Samuel Resendez <saresend@usc.edu>
2017-07-23 22:49:37 +0300

    sorted to make it a bit more sensible

--- sheen.py
+++ sheen.py
@@ -2,7 +2,7 @@ import re
 import os
 import sys
 
-def parseFile(file_path):
+def parseFile(file_path, endDict):
     foundTodo = False
     f = open(file_path, 'r')
     todoReg = re.compile(r'(.*)(TODO.+)')
@@ -12,7 +12,9 @@ def parseFile(file_path):
             match = todoReg.match(line)
             if match != None:
                 foundTodo = True
-                print('\tLine ' + str(count) + ': ' + match.group(2))
+                if file_path not in endDict:
+                    endDict[file_path] = []
+                endDict[file_path].append('\tLine ' + str(count) + ': ' + match.group(2))
             count += 1
     except UnicodeDecodeError as e:
         pass
@@ -21,23 +23,30 @@ def parseFile(file_path):
         print("No TODOs Found")
 
 
-def scan_directory(dirName):
+def scan_directory(dirName, endDict):
+   
     try:
         directory = os.scandir(dirName)
         for fileObj in directory:
             if fileObj.is_dir():
-                scan_directory(fileObj.path)
+                scan_directory(fileObj.path, endDict)
             elif fileObj.is_file():
-                print(fileObj.path)
-                parseFile(fileObj.path)
+                parseFile(fileObj.path, endDict)
 
     except NotADirectoryError as e:
-        parseFile(dirName)
+        parseFile(dirName, endDict)
+
+    
     
 
 if len(sys.argv) < 2:
     print("Please enter file or directory name")
     exit()
 else: 
-    scan_directory(sys.argv[1])
+    endDict = {}
+    scan_directory(sys.argv[1], endDict)
+    for k, v in endDict.items():
+        print(k)
+        for line in v:
+            print(line)
 
